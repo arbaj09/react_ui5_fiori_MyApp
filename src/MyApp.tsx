be97e45b6e88103546682442f7dadd9b8a4ccf3b
arbaj shekh <shaikharbaj092@gmail.com>
2024-11-27 12:10:03 +0530

    first commit

---
+++ src/MyApp.tsx
@@ -0,0 +1,29 @@
+import { Card ,CardHeader,Text } from "@ui5/webcomponents-react";
+
+import { spacing } from "@ui5/webcomponents-react-base";
+
+
+export function MyApp() {
+
+const handleHeaderClick =()=>{
+  alert('header clicked')
+}
+
+    return(
+    
+    
+    <div>
+    <Card header={<CardHeader titleText="Card" interactive  onClick={handleHeaderClick} /> } style={{ width: "300px" }}>
+      <Text style={spacing.sapUiContentPadding}>
+        This is the content area of the Card
+        </Text>
+    </Card>
+  </div>
+    )
+  
+    
+
+  
+     
+  }
+  
