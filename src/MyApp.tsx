5364b4bf5b528a46b8a4a82af500e72d4ceefadb
arbaj shekh <shaikharbaj092@gmail.com>
2024-11-27 13:08:59 +0530

    adding chart lineChart , BarChart and conditional rendring

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,22 +1,103 @@
-import { Card ,CardHeader,Text } from "@ui5/webcomponents-react";
+import { Card ,CardHeader,Text ,Icon } from "@ui5/webcomponents-react";
 
 import { spacing } from "@ui5/webcomponents-react-base";
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+import { useState } from "react";
+import lineChartIcon from '@ui5/webcomponents-icons/dist/line-chart.js';
+import barChartIcon from '@ui5/webcomponents-icons/dist/horizontal-bar-chart.js';
+
+
 
 
 export function MyApp() {
+  const [ToggleChart,setToggleChart] = useState('lineChart')
+  const [Loading,setLoading] = useState(false)
+  const dataset = [
+    {
+      month: "January",
+      data: 65
+    },
+    {
+      month: "February",
+      data: 59
+    },
+    {
+      month: "March",
+      data: 80
+    },
+    {
+      month: "April",
+      data: 81
+    },
+    {
+      month: "May",
+      data: 56
+    },
+    {
+      month: "June",
+      data: 55
+    },
+    {
+      month: "July",
+      data: 40
+    }
+  ];
+  
 
 const handleHeaderClick =()=>{
-  alert('header clicked')
+  if(ToggleChart==='lineChart'){
+    setToggleChart('barChart')
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      setToggleChart('barChart')
+      
+    }, 2000);
+
+  }else
+  {
+    setToggleChart('lineChart')
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)   
+      setToggleChart('lineChart')   
+    }, 2000);
+  }
+  
 }
 
     return(
     
     
     <div>
-    <Card header={<CardHeader titleText="Card" interactive  onClick={handleHeaderClick} /> } style={{ width: "300px" }}>
+    <Card
+     header={
+     <CardHeader titleText="Card" interactive  onClick={handleHeaderClick}
+     avatar={ <Icon name={ ToggleChart === "lineChart" ? lineChartIcon : barChartIcon } /> }
+
+     /> } style={{ width: "300px" }}>
       <Text style={spacing.sapUiContentPadding}>
         This is the content area of the Card
         </Text>
+
+        {ToggleChart ==="lineChart" ? (
+          <LineChart 
+        dimensions={[{ accessor: "month" }]}
+        measures={[{ accessor: "data", label: "Stock Price" }]}
+        dataset={dataset}
+        loading={Loading}
+         />
+        ):
+          ( 
+          <BarChart
+          dimensions={[{ accessor: "month" }]}
+          measures={[{ accessor: "data", label: "Stock Price" }]}
+          dataset={dataset}
+          loading={Loading}
+        />
+          )
+}
+
     </Card>
   </div>
     )
